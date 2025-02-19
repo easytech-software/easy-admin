eb868cdccebf549f99739f46da6a61527bb3fc6f
Matheus Feitosa <matheus.feitosa@defensoria.ce.def.br>
2025-02-18 22:28:58 -0300

    commit inicial

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+@login_required
+def index(request):
+
+    # Page from the theme 
+    return render(request, 'pages/index.html')
